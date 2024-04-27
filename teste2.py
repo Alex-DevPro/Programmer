79559fa5324a56f299cb389071785151fc9ebc3a
Alex Rocha <67865578+Alex-DevPro@users.noreply.github.com>
2024-04-27 20:15:16 -0300

    Delete teste2.py

--- teste2.py
+++
@@ -1,9 +0,0 @@
-palavra = input("Qual a palavra: ")
-qt = 0
-
-for palavras in palavra:
-    if palavras in "aeiou":
-        qt += 1
-        print("Quantidade de vogais: ", palavras)
-
-# print("Quantidade de vogais: ", qt)
