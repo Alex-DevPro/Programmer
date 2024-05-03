dddd0a4b24d5312b764edc41c04ca2f1777b664e
Alex Rocha <67865578+Alex-DevPro@users.noreply.github.com>
2024-05-03 10:38:26 -0300

    Dias vivido

---
+++ diasvivido.py
@@ -0,0 +1,7 @@
+anos = int(input("Quantos anos você tem: "))
+meses = int(input("Quantos meses de idade você tem: "))
+dias = int(input("Quantos dias de idade você tem: "))
+
+resultado = anos * 365 + meses * 30 + dias
+
+print("você ja viveu ", resultado, "dias!")
